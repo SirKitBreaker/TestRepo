5eab84f3790ae056c28dcfd52fdd0c788c3f8fdf
SirKitBreaker <saahil@cklabs.com>
2025-06-11 17:03:29 +0530

    Added 8 java activities

---
+++ Java/StringisEmptyException.java
@@ -0,0 +1,14 @@
+package activities;
+
+public class StringisEmptyException extends Exception {
+    private String message;
+
+    public StringisEmptyException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return "The error happens because: " + this.message;
+    }
+}
